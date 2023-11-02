4e044317288b03c17e26cb99e370116978f03171
stge35 <evanok1@gmail.com>
2023-11-02 22:04:39 +0900

    Add assert method test

---
+++ src/test/java/app/practice_tdd/QuickBeforeAfterTest.java
@@ -0,0 +1,4 @@
+package app.practice_tdd;
+
+public class QuickBeforeAfterTest {
+}
--- src/test/java/app/practice_tdd/StringHelperTest.java
+++ src/test/java/app/practice_tdd/StringHelperTest.java
@@ -7,9 +7,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringHelperTest {
 
+    StringHelper stringHelper = new StringHelper();
+
     @Test
-    public void test() {
-        assertEquals("ABC", "ABCD");
+    public void truncateAInFirst2Positions_AinFirst2Positions_Test() {
+        assertEquals("CD", stringHelper.truncateAInFirst2Positions("AACD"));
+        // 하나의 단위 테스트에 2가지 테스트는 좋지 않은 방법
+    }
+
+    @Test
+    public void truncateAInFirst2PositionsTest2_AinFirst1Positions_Test() {
+        assertEquals("CD", stringHelper.truncateAInFirst2Positions("ACD"));
+        // Test는 공개적인 void 타입으로 해야함. 특정 반환타입은 안됨
+    }
+
+    @Test
+    public void areFirstAndLastTwoCharactersTheSame__BasicNegativeScenario_Test() {
+        assertFalse(stringHelper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+    }
+
+    @Test
+    public void areFirstAndLastTwoCharactersTheSame__BasicPositiveScenario_Test() {
+        assertTrue(stringHelper.areFirstAndLastTwoCharactersTheSame("ABAB"));
     }
 
 }
