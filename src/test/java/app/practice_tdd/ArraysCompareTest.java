39c03e98ab478f1bd235287554b8d0b8988c8bc9
stge35 <evanok1@gmail.com>
2023-11-02 23:00:14 +0900

    Add Exception Test in jUnit5

--- src/test/java/app/practice_tdd/ArraysCompareTest.java
+++ src/test/java/app/practice_tdd/ArraysCompareTest.java
@@ -1,8 +1,31 @@
 package app.practice_tdd;
 
 import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ArraysCompareTest {
 
+    @Test
+    public void ArraySortRandom_Test() {
+        int[] numbers = {12, 3, 4, 1};
+        int[] expected = {1, 3, 4, 12};
+        Arrays.sort(numbers);
+        assertArrayEquals(expected, numbers);
+    }
 
+    @Test
+    public void ArraySortNullArray_Test() {
+
+        assertThrows(NullPointerException.class, () -> {
+            int[] numbers = null;
+            Arrays.sort(numbers);
+        });
+
+        // 예측 되는 에러.class -> catch -> throw
+
+
+    }
 }
