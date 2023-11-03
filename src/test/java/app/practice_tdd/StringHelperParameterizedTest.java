66d7267fd15bc714a48395d473427d7568c5d723
stge35 <evanok1@gmail.com>
2023-11-03 18:07:55 +0900

    Add Parameterized Test

--- src/test/java/app/practice_tdd/StringHelperParameterizedTest.java
+++ src/test/java/app/practice_tdd/StringHelperParameterizedTest.java
@@ -2,31 +2,43 @@ package app.practice_tdd;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Collection;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
+
 class StringHelperParameterizedTest {
 
-    StringHelper stringHelper;
+    StringHelper stringHelper = new StringHelper();
 
-    @BeforeEach
-    public void beforeEach() {
-        stringHelper = new StringHelper();
-        System.out.println("test eachBefore Annotation");
+
+    // ParameterizedTest
+    // MethodSource 를 사용 시 Argument 의 순서 주의 할 것
+    // 첫 번 째가 처음으로 들어간다!!
+    private static Stream<Arguments> testArguments() {
+        return Stream.of(
+                Arguments.of("CD", "AACD"),
+                Arguments.of("CD", "ACD")
+        );
     }
 
-    @Test
-    public void truncateAInFirst2Positions_AinFirst2Positions_Test() {
-        assertEquals("CD", stringHelper.truncateAInFirst2Positions("AACD"));
-        // 하나의 단위 테스트에 2가지 테스트는 좋지 않은 방법
+    @ParameterizedTest
+    @MethodSource("testArguments")
+    // MethodSource 애너테이션에서는 생성자 주입을 할 필요가 없음.
+    public void truncateAInFirst2Positions_Test(String expectedOutput, String input) {
+
+        assertEquals(expectedOutput, stringHelper.truncateAInFirst2Positions(input));
     }
 
-    @Test
-    public void truncateAInFirst2PositionsTest2_AinFirst1Positions_Test() {
-        assertEquals("CD", stringHelper.truncateAInFirst2Positions("ACD"));
-        // Test는 공개적인 void 타입으로 해야함. 특정 반환타입은 안됨
-    }
 
     @Test
     public void areFirstAndLastTwoCharactersTheSame__BasicNegativeScenario_Test() {
